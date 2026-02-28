08ad4ee86d8a13b36e133bfa6b1fdb304ae0a506
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-02-28 11:46:23 -0600

    Refactor TDA class structure and formatting

--- TDA.java
+++ TDA.java
@@ -6,8 +6,7 @@ private String Email;
 private String CatContacto;
 private boolean Fav;
 private String FotoContacto;
-public TDA() {
-    }
+public TDA() {}
 public TDA(String NomContacto, String NumContacto) {
 this.NomContacto = NomContacto;
 this.NumContacto = NumContacto; 
@@ -20,45 +19,21 @@ this.CatContacto = CatContacto;
 this.Fav = Fav;
 this.FotoContacto = FotoContacto;
 }
-public void setNomContacto(String NomContacto) {
-this.NomContacto = NomContacto;
-}
-public void setNumContacto(String NumContacto) {
-this.NumContacto = NumContacto;
-}
-public void setEmail(String Email) {
-this.Email = Email;
-}
-public void setCatContacto(String CatContacto) {
-this.CatContacto = CatContacto;
-}
-public void setFav(boolean Fav) {
-this.Fav = Fav;
-}
-public void setFotoContacto(String FotoContacto) {
-this.FotoContacto = FotoContacto;
-} 
+public void setNomContacto(String NomContacto) {this.NomContacto = NomContacto;}
+public void setNumContacto(String NumContacto) {this.NumContacto = NumContacto;}
+public void setEmail(String Email) {this.Email = Email;}
+public void setCatContacto(String CatContacto) {this.CatContacto = CatContacto;}
+public void setFav(boolean Fav) {this.Fav = Fav;}
+public void setFotoContacto(String FotoContacto) {this.FotoContacto = FotoContacto;} 
 @Override
 public String toString() {
 return "TDA{"+"Nombre de Contacto = "+NomContacto+",Numero de Contacto = "+NumContacto+",Email = "+
 Email+",Categoria de Contacto = "+CatContacto+",Favorito = "+Fav+",Foto de Contacto = "+FotoContacto+'}';
 }
-public String getNomContacto() {
-        return NomContacto;
-    }
-public String getNumContacto() {
-        return NumContacto;
-    }
-public String getEmail() {
-return Email;
-}
-public String getCatContacto() {
-return CatContacto;
-}
-public boolean isFav() {
-return Fav;
-}
-public String getFotoContacto() {
-return FotoContacto;
-}
+public String getNomContacto() {return NomContacto;}
+public String getNumContacto() {return NumContacto;}
+public String getEmail() {return Email;}
+public String getCatContacto() {return CatContacto;}
+public boolean isFav() {return Fav;}
+public String getFotoContacto() {return FotoContacto;}
 }
