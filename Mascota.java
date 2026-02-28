9fc863376c7cc038f29ed3ad83255eda04d39eb4
yahir <71457896+hypergecko491@users.noreply.github.com>
2026-02-28 11:45:47 -0600

    Refactor Mascota class with additional getters and toString

--- Mascota.java
+++ Mascota.java
@@ -13,8 +13,7 @@ private boolean Esterilizado;
 private String SaludMascota;
 private float PCompraMascota;
 private float PVentaMascota;
-public Mascota() {
-    }
+public Mascota() {}
 public Mascota(short ID, String NMascota, String EMascota, short FNMascota, String GMascota, float PMascota, String CPMascota, boolean Vacunado, boolean Desparasitado, boolean Esterilizado, String SaludMascota, float PCompraMascota, float PVentaMascota) {
         this.ID = ID;
         this.NMascota = NMascota;
@@ -30,45 +29,19 @@ public Mascota(short ID, String NMascota, String EMascota, short FNMascota, Stri
         this.PCompraMascota = PCompraMascota;
         this.PVentaMascota = PVentaMascota;
     }
-public void setID(short ID) {
-        this.ID = ID;
-    }
-public void setNMascota(String NMascota) {
-        this.NMascota = NMascota;
-    }
-public void setEMascota(String EMascota) {
-        this.EMascota = EMascota;
-    }
-public void setFNMascota(short FNMascota) {
-        this.FNMascota = FNMascota;
-    }
-public void setGMascota(String GMascota) {
-        this.GMascota = GMascota;
-    }
-public void setPMascota(float PMascota) {
-        this.PMascota = PMascota;
-    }
-public void setCPMascota(String CPMascota) {
-        this.CPMascota = CPMascota;
-    }
-public void setVacunado(boolean Vacunado) {
-        this.Vacunado = Vacunado;
-    }
-public void setDesparasitado(boolean Desparasitado) {
-this.Desparasitado = Desparasitado;
-    }
-public void setEsterilizado(boolean Esterilizado) {
-        this.Esterilizado = Esterilizado;
-    }
-public void setSaludMascota(String SaludMascota) {
-        this.SaludMascota = SaludMascota;
-    }
-public void setPCompraMascota(float PCompraMascota) {
-        this.PCompraMascota = PCompraMascota;
-    }
-public void setPVentaMascota(float PVentaMascota) {
-        this.PVentaMascota = PVentaMascota;
-    }
+public void setID(short ID) {this.ID = ID;}
+public void setNMascota(String NMascota) {this.NMascota = NMascota;}
+public void setEMascota(String EMascota) {this.EMascota = EMascota;}
+public void setFNMascota(short FNMascota) {this.FNMascota = FNMascota;}
+public void setGMascota(String GMascota) {this.GMascota = GMascota;}
+public void setPMascota(float PMascota) {this.PMascota = PMascota;}
+public void setCPMascota(String CPMascota) {this.CPMascota = CPMascota;}
+public void setVacunado(boolean Vacunado) {this.Vacunado = Vacunado;}
+public void setDesparasitado(boolean Desparasitado) {this.Desparasitado = Desparasitado;}
+public void setEsterilizado(boolean Esterilizado) {this.Esterilizado = Esterilizado;}
+public void setSaludMascota(String SaludMascota) {this.SaludMascota = SaludMascota;}
+public void setPCompraMascota(float PCompraMascota) {this.PCompraMascota = PCompraMascota;}
+public void setPVentaMascota(float PVentaMascota) {this.PVentaMascota = PVentaMascota;}
 public Mascota(short ID, String NMascota, String EMascota, String GMascota, float PMascota, boolean Vacunado, boolean Desparasitado, boolean Esterilizado, String SaludMascota) {
         this.ID = ID;
         this.NMascota = NMascota;
@@ -96,5 +69,16 @@ public String getEMascota(){ return EMascota; }
 public float getPMascota(){ return PMascota; }
 public String getCPMascota(){ return CPMascota; }
 public String getSaludMascota(){ return SaludMascota; }
-}
+public String getGMascota() {return GMascota;}
+public short getID() {return ID;}
+public short getFNMascota() {return FNMascota;}
+public boolean isDesparasitado() {return Desparasitado;}
+public boolean isEsterilizado() {return Esterilizado;}
+public float getPCompraMascota() {return PCompraMascota;}
+public float getPVentaMascota() {return PVentaMascota;}
+@Override
+public String toString() {
+        return "Mascota{" + "ID=" + ID + ", NMascota=" + NMascota + ", EMascota=" + EMascota + ", FNMascota=" + FNMascota + ", GMascota=" + GMascota + ", PMascota=" + PMascota + ", CPMascota=" + CPMascota + ", Vacunado=" + Vacunado + ", Desparasitado=" + Desparasitado + ", Esterilizado=" + Esterilizado + ", SaludMascota=" + SaludMascota + ", PCompraMascota=" + PCompraMascota + ", PVentaMascota=" + PVentaMascota + '}';
+    }
 
+}
